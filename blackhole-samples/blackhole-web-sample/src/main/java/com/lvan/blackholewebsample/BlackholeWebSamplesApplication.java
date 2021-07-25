57b771fdc09307fe9f84e8e27aeaee47220c8dc0
LvanLiu <155333767@qq.com>
2021-07-25 12:42:10 +0800

    项目创建

---
+++ blackhole-samples/blackhole-web-sample/src/main/java/com/lvan/blackholewebsample/BlackholeWebSamplesApplication.java
@@ -0,0 +1,12 @@
+package com.lvan.blackholewebsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackholeWebSamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackholeWebSamplesApplication.class, args);
+    }
+}
