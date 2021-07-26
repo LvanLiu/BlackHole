21df48f45150032a85709f359ac9e9f80a4d0231
LvanLiu <155333767@qq.com>
2021-07-26 23:01:08 +0800

    集成redis

--- blackhole-samples/blackhole-spring-cloud-service-provider/src/main/java/com/lvan/blackholespringcloudserviceprovider/controller/UserController.java
+++ blackhole-samples/blackhole-spring-cloud-service-provider/src/main/java/com/lvan/blackholespringcloudserviceprovider/controller/UserController.java
@@ -1,14 +1,18 @@
 package com.lvan.blackholespringcloudserviceprovider.controller;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.lvan.blackholespringcloudserviceprovider.entity.User;
 import com.lvan.blackholespringcloudserviceprovider.mapper.UserMapper;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Lvan
@@ -20,10 +24,15 @@ public class UserController {
 
     @Resource
     private UserMapper userMapper;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     @GetMapping("listAll")
     public List<User> listAllUsers() {
-
-        return userMapper.selectList(Wrappers.emptyWrapper());
+        List<User> userList = userMapper.selectList(Wrappers.emptyWrapper());
+        if (CollUtil.isNotEmpty(userList)) {
+            stringRedisTemplate.opsForValue().set("userList", JSONUtil.toJsonStr(userList), 5, TimeUnit.MINUTES);
+        }
+        return userList;
     }
 }
